cf724a404a94e1d75d5afdeb91442e2794ad466f
carlos2015matus <charly26763@hotmail.com>
2015-11-24 20:12:24 -0600

    Actualizacion
    Actualizacion MAtus 08:10 pm

---
+++ DatViajes/DatUsuario.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AgenciaViaje.Web.Data
+{
+    class DatUsuario
+    {
+    }
+}
---
+++ EntViajes/EntUsuario.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AgenciaViaje.Web.Business.Entity
+{
+    class EntUsuario
+    {
+    }
+}
